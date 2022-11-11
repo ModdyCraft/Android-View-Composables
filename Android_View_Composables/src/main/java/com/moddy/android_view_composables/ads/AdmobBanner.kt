02310cf751bb203d1bd5aa9328edfbf3fdb77c2e
ModdyCraft <romeroeinsten@gmail.com>
2022-11-10 23:17:17 -0500

    SecondVersion

--- Android_View_Composables/src/main/java/com/moddy/android_view_composables/ads/AdmobBanner.kt
+++ Android_View_Composables/src/main/java/com/moddy/android_view_composables/ads/AdmobBanner.kt
@@ -5,23 +5,44 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.moddy.android_view_composables.vars.AdBannerSize
 
 @Composable
 fun AdmobBanner(
     modifier: Modifier = Modifier,
     adUnitId: String,
-    adSize: AdSize,
+    adSize: AdBannerSize,
     config: (AdView) -> Unit = {},
 ) {
+    val mapAD = hashMapOf<AdBannerSize, AdSize>(
+        AdBannerSize.BANNER to AdSize.BANNER,
+        AdBannerSize.FULL_BANNER to AdSize.FULL_BANNER,
+        AdBannerSize.LARGE_BANNER to AdSize.LARGE_BANNER,
+        AdBannerSize.SMART_BANNER to AdSize.SMART_BANNER,
+        AdBannerSize.FLUID to AdSize.FLUID,
+        AdBannerSize.INVALID to AdSize.INVALID,
+        AdBannerSize.LEADERBOARD to AdSize.LEADERBOARD,
+        AdBannerSize.MEDIUM_RECTANGLE to AdSize.MEDIUM_RECTANGLE,
+        AdBannerSize.SEARCH to AdSize.SEARCH,
+        AdBannerSize.WIDE_SKYSCRAPER to AdSize.WIDE_SKYSCRAPER,
+        AdBannerSize.zza to AdSize.zza
+    )
+
+    ADVIEWC(adSize = mapAD[adSize], adUnitId = adUnitId, config = {config(it)}, modifier = modifier)
+}
+
+@Composable
+private fun ADVIEWC(adSize: AdSize?, adUnitId: String, config: (AdView) -> Unit, modifier: Modifier = Modifier) {
     AndroidView(
         factory = {
-        AdView(it).apply {
-            this.setAdSize(adSize)
-            this.adUnitId = adUnitId
-            config(this)
-        }
-
-    },
-    modifier =  modifier
+            AdView(it).apply {
+                if (adSize != null) {
+                    this.setAdSize(adSize)
+                }
+                this.adUnitId = adUnitId
+                config(this)
+            }
+        },
+        modifier =  modifier
     )
 }
---
+++ Android_View_Composables/src/main/java/com/moddy/android_view_composables/vars/AdBannerSize.kt
@@ -0,0 +1,15 @@
+package com.moddy.android_view_composables.vars
+
+enum class AdBannerSize {
+    BANNER,
+    FULL_BANNER,
+    LARGE_BANNER,
+    SMART_BANNER,
+    FLUID,
+    INVALID,
+    LEADERBOARD,
+    MEDIUM_RECTANGLE,
+    SEARCH,
+    WIDE_SKYSCRAPER,
+    zza
+}
